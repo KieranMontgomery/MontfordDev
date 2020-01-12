99a355da5ed939cde5cc74c3b2327c16ef023db7
John Stafford <johnstaf144@gmail.com>
2020-01-12 21:27:55 +0000

    Addeed gravity

--- John/Unity/JohnDev/Assets/PlayerMovement.cs
+++ John/Unity/JohnDev/Assets/PlayerMovement.cs
@@ -8,15 +8,22 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+    public float gravity = -9.81f;
+
+    Vector3 velocity;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float movHorizontal = Input.GetAxis("Horizontal");
-        float movVertical = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * movHorizontal + transform.forward * movVertical;
+        Vector3 move = transform.right * x + transform.forward * y;
 
         controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
